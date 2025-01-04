3cb1c5d1aaa2e135406980ae7e5f729d64bbadc1
Lister <75606909+lister-developer@users.noreply.github.com>
2025-01-04 23:30:17 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,6 +26,8 @@ let cxxSettings: [CXXSetting] = [
     .define("REALM_INSTALL_LIBEXECDIR", to: ""),
     .define("REALM_ENABLE_ASSERTIONS", to: "1"),
     .define("REALM_ENABLE_ENCRYPTION", to: "1"),
+    .define("REALM_DISABLE_ANALYTICS", to: "1"),
+    
 
     .define("REALM_VERSION_MAJOR", to: String(coreVersion.major)),
     .define("REALM_VERSION_MINOR", to: String(coreVersion.minor)),
